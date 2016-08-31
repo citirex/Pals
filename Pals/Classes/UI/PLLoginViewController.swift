3e991588cc6aab14fe0907e6cf634b4b32b37eba
ruckef <vladislav.senyukov@gmail.com>
2016-08-31 10:59:44 +0300

    added prefix, empty login view controller

---
+++ Pals/Classes/UI/PLLoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PLLoginViewController.swift
+//  Pals
+//
+//  Created by ruckef on 31.08.16.
+//  Copyright © 2016 citirex. All rights reserved.
+//
+
+import UIKit
+
+class PLLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
