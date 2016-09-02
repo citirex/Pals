a104d788cc2a0a6d9c9ec9da32f63dba92fe8cac
skrello <qweqwenet@gmail.com>
2016-09-02 14:22:03 +0300

    change server error in login screen

--- Pals/Classes/UI/PLLoginViewController.swift
+++ Pals/Classes/UI/PLLoginViewController.swift
@@ -28,13 +28,14 @@ class PLLoginViewController: UIViewController {
             // show error message
 			alertCalled("Please enter your password.")
         } else {
-			self.spinner = UIActivityIndicatorView(frame: CGRectMake(self.view.bounds.width / 2, self.view.bounds.height / 2 - 35, 0, 0)) as UIActivityIndicatorView
-			self.view.addSubview(self.spinner!)
-			self.spinner!.startAnimating()
+			spinner = UIActivityIndicatorView(frame: CGRectMake(view.bounds.width / 2, self.view.bounds.height / 2 - 35, 0, 0)) as UIActivityIndicatorView
+			view.addSubview(spinner!)
+			spinner!.startAnimating()
             PLFacade.login(userName, password: password, completion: { (error) in
                 if error != nil {
                     // show error message
-					self.alertCalled("Wrong user name or password.")
+					self.alertCalled((error?.localizedDescription)!)
+					self.spinner?.stopAnimating()
                 } else {
                     self.showMainScreen()
 					self.spinner?.stopAnimating()
@@ -43,7 +44,7 @@ class PLLoginViewController: UIViewController {
         }
 	}
 	@IBAction func forgotButtonClicked(sender: AnyObject) {
-		self.spinner = UIActivityIndicatorView(frame: CGRectMake(self.view.bounds.width / 2, self.view.bounds.height / 2 - 35, 0, 0)) as UIActivityIndicatorView
+		self.spinner = UIActivityIndicatorView(frame: CGRectMake(view.bounds.width / 2, view.bounds.height / 2 - 35, 0, 0)) as UIActivityIndicatorView
 		let alert = UIAlertController(title: "We got your back!", message: "Enter below and we'll send your password!", preferredStyle: UIAlertControllerStyle.Alert)
 		alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil))
 		alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: {
@@ -54,7 +55,7 @@ class PLLoginViewController: UIViewController {
 		alert.addTextFieldWithConfigurationHandler({ (textField) -> Void in
 			textField.placeholder = "Email"
 		})
-		self.presentViewController(alert, animated: true, completion: nil)
+		presentViewController(alert, animated: true, completion: nil)
 	}
 	@IBAction func registerButtonClicked(sender: AnyObject) {
 	}
@@ -70,7 +71,7 @@ class PLLoginViewController: UIViewController {
 	func showMainScreen() {
 		let storyboard = UIStoryboard(name: "Main", bundle: nil)
 		let vc = storyboard.instantiateViewControllerWithIdentifier("TabBarController")
-		self.presentViewController(vc, animated: true, completion: nil)
+		presentViewController(vc, animated: true, completion: nil)
 	}
 	
 	override func preferredStatusBarStyle() -> UIStatusBarStyle {
@@ -89,16 +90,16 @@ class PLLoginViewController: UIViewController {
 		viewAppearLogo(logoImage, flag: true)
 		viewAppearAnimation(animationView, duration: 1, delay: 0.2, flag: true)
 		
-		self.hideKeyboardWhenTappedAround()
+		hideKeyboardWhenTappedAround()
     }
 	func viewAppearAnimation(view: UIView, duration:NSTimeInterval, delay: NSTimeInterval, flag:Bool) {
 		view.alpha = 0.0
 		UIView.animateWithDuration(duration, delay: delay, options: .CurveEaseOut, animations: {
 			view.alpha = 1.0
 			if flag{
-				view.center.y -= self.view.bounds.height
+				view.center.y -= view.bounds.height
 			}else{
-				view.center.y += self.view.bounds.height
+				view.center.y += view.bounds.height
 			}
 			}, completion: {_ in
 		})
@@ -110,7 +111,7 @@ class PLLoginViewController: UIViewController {
 			if flag{
 				view.center.y = 0
 			}else{
-				view.center.y += self.view.bounds.height
+				view.center.y += view.bounds.height
 			}
 			}, completion: {_ in
 		})
