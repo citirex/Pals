49b2ca4b7a4267951ce7507205636a836ebf09d6
jam891 <jam891@gmail.com>
2016-09-05 14:15:16 +0300

    added friend profile scene

---
+++ Pals/Classes/UI/PLFriendProfileViewController.swift
@@ -0,0 +1,38 @@
+//
+//  PLFriendProfileViewController.swift
+//  Pals
+//
+//  Created by Vitaliy Delidov on 9/5/16.
+//  Copyright © 2016 citirex. All rights reserved.
+//
+
+import UIKit
+
+class PLFriendProfileViewController: UIViewController {
+
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
