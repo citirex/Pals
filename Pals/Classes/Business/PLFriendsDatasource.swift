76c1e832b859711dd27fc44189e8b1c36aa2192b
ruckef <vladislav.senyukov@gmail.com>
2016-09-07 16:03:19 +0300

    Fixed autoselection paths on file creation

---
+++ Pals/Classes/Business/PLFriendsDatasource.swift
@@ -0,0 +1,11 @@
+//
+//  PLFriendsDatasource.swift
+//  Pals
+//
+//  Created by ruckef on 07.09.16.
+//  Copyright © 2016 citirex. All rights reserved.
+//
+
+class PLFriendsDatasource: NSObject {
+
+}
