0905235500ba8541940f16bc1790f131b14a58c1
jam891 <jam891@gmail.com>
2016-09-09 18:34:29 +0300

    Added calculate distance method

--- Pals/Classes/UI/Places/PLPlaceTableViewCell.swift
+++ Pals/Classes/UI/Places/PLPlaceTableViewCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class PLPlaceTableViewCell: UITableViewCell {
     
@@ -29,7 +30,21 @@ class PLPlaceTableViewCell: UITableViewCell {
         placeNameLabel.text = placeCellData.name
         placeAddressLabel.text = placeCellData.address
         musicGenresLabel.text = placeCellData.musicGengres
-//        distanceLabel.text = placeCellData.distance
+        distanceLabel.text = String(format: "%.1f Miles", distance(placeCellData.location))
     }
 
+    private func distance(destination: CLLocationCoordinate2D) -> Double {
+        let locationManager = PLLocationManager()
+        guard let currentLocation = locationManager.currentLocation else { return 0 }
+        let placeLocation = CLLocation(latitude: destination.latitude, longitude: destination.longitude)
+        let distance = currentLocation.distanceFromLocation(placeLocation).miles()
+        return distance
+    }
+}
+
+extension CLLocationDistance {
+
+    func miles() -> Double {
+        return self / 1609.34
+    }
 }
