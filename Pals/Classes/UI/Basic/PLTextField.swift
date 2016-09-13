a6069c6ca25f49d1a554a2abdd3c97a56b563feb
jam891 <jam891@gmail.com>
2016-09-13 22:51:11 +0300

    added drawRect to PLTextField

--- Pals/Classes/UI/Basic/PLTextField.swift
+++ Pals/Classes/UI/Basic/PLTextField.swift
@@ -11,30 +11,26 @@ import UIKit
 @IBDesignable
 class PLTextField: UITextField {
     
-    @IBInspectable var borderWidth: CGFloat = 0 {
-        didSet {
-            let separator = UIView(frame: CGRect(x: 0.0, y: bounds.height, width: bounds.width, height: borderWidth))
-            separator.translatesAutoresizingMaskIntoConstraints = false
-            separator.backgroundColor = separatorColor
-            addSubview(separator)
-            
-            let views = ["line" : separator]
-            let metrics = ["lineWidth" : borderWidth]
-            addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("|[line]|", options: [], metrics: nil, views: views))
-            addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[line(==lineWidth)]|", options: [], metrics: metrics, views: views))
-        }
-    }
-    
-    @IBInspectable var separatorColor: UIColor? {
-        didSet {
-            layer.borderColor = separatorColor?.CGColor
-        }
-    }
-    
+    @IBInspectable var separatorWidth: CGFloat = 0
+    @IBInspectable var separatorColor: UIColor = .clearColor()
     @IBInspectable var placeHolderColor: UIColor? {
         didSet {
-            attributedPlaceholder = NSAttributedString(string: placeholder != nil ? placeholder! : "", attributes: [NSForegroundColorAttributeName : placeHolderColor!])
+            attributedPlaceholder =
+                NSAttributedString(string: placeholder != nil ? placeholder! : "",
+                attributes: [NSForegroundColorAttributeName : placeHolderColor!])
         }
+    }
+    
+    override func drawRect(rect: CGRect) {
+        let startingPoint = CGPoint(x: rect.minX, y: rect.maxY)
+        let endingPoint = CGPoint(x: rect.maxX, y: rect.maxY)
+        
+        let path = UIBezierPath()
+        path.moveToPoint(startingPoint)
+        path.addLineToPoint(endingPoint)
+        path.lineWidth = separatorWidth
+        separatorColor.setStroke()
+        path.stroke()
     }
 
 }
