e38044e3fd86e3a95ba605fd9f96c5293cb79e4f
jam891 <jam891@gmail.com>
2016-09-30 10:51:02 +0300

    updated PLDate extension

--- Pals/Classes/Extensions/PLDate.swift
+++ Pals/Classes/Extensions/PLDate.swift
@@ -8,59 +8,88 @@
 
 import Foundation
 
-enum PLDateType {
-    case YearsAgo(Int), MonthsAgo(Int), WeeksAgo(Int), DaysAgo(Int), LastYear, LastMonth, LastWeek, Yesterday, Today
+enum PLDateComponent: Int {
+    case YearsAgo
+    case MonthsAgo
+    case WeeksAgo
+    case DaysAgo
+    case LastYear
+    case LastMonth
+    case LastWeek
+    case Yesterday
+    case Today
+}
+
+extension PLDateComponent {
     
     var description: String {
         switch self {
-        case .YearsAgo(let amount):  return String(format: "%d Years Ago", amount)
-        case .MonthsAgo(let amount): return String(format: "%d Months Ago", amount)
-        case .WeeksAgo(let amount):  return String(format: "%d Weeks Ago", amount)
-        case .DaysAgo(let amount):   return String(format: "%d Days Ago", amount)
-        case .LastYear:              return "Last Year"
-        case .LastMonth:             return "Last Month"
-        case .LastWeek:              return "Last Week"
-        case .Yesterday:             return "Yesterday"
-        case .Today:                 return "Today"
+        case .YearsAgo:  return ""
+        case .MonthsAgo: return ""
+        case .WeeksAgo:  return ""
+        case .DaysAgo:   return ""
+        case .LastYear:  return ""
+        case .LastMonth: return ""
+        case .LastWeek:  return ""
+        case .Yesterday: return ""
+        case .Today:     return ""
         }
     }
 }
 
 
 
+
+
 extension NSDate {
     
-    var since: String {
-        let calendar = NSCalendar.currentCalendar()
-        let now = NSDate()
-        let earliest = now.earlierDate(self)
-        let latest = earliest == now ? self : now
-        
-        let components = calendar.components([.Day, .WeekOfYear, .Month, .Year], fromDate: earliest, toDate: latest, options: NSCalendarOptions())
-        
-        if components.year >= 2 { return "\(components.year) Years Ago" }
-        else if components.year >= 1 { return "LastYear" }
-            
-        else if components.month >= 2 { return "\(components.month) Months Ago" }
-        else if components.month >= 1 { return "LastMonth" }
-            
-        else if components.weekOfYear >= 2 { return "\(components.weekOfYear) Weeks Ago" }
-        else if components.weekOfYear >= 1 { return "LastWeek" }
-            
-        else if components.day >= 2 { return "\(components.day) Days Ago" }
-        else if components.day >= 1 { return "Yesterday" }
-
-        else { return "Today" }
-    }
-
-    
-    
-    private func dateComponents() -> NSDateComponents {
+    private var dateComponents: NSDateComponents {
         let calander = NSCalendar.currentCalendar()
-        return calander.components([.Day, .WeekOfYear, .Month, .Year], fromDate: self, toDate: NSDate(), options: [])
+        return calander.components([.Day, .Month, .Year], fromDate: self, toDate: NSDate(), options: [])
     }
     
     
+    var since: String {
+        
+        if dateComponents.year > 0 {
+            if dateComponents.year < 2 {
+                return "Last year"
+            } else {
+                return String(format: "%d Years Ago", dateComponents.year)
+            }
+        }
+        
+        if dateComponents.month > 0 {
+            if dateComponents.month < 2 {
+                return "Last month"
+            } else {
+                return String(format: "%d Months Ago", dateComponents.month)
+            }
+        }
+
+        if dateComponents.day >= 7 {
+            let week = dateComponents.day / 7
+            if week < 2 {
+                return "Last week"
+            } else {
+                return String(format: "%d Weeks Ago", week)
+            }
+        }
+            
+        if dateComponents.day > 0 {
+            if dateComponents.day < 2 {
+                return "Yesterday"
+            } else  {
+                return String(format: "%d Days Ago", dateComponents.day)
+            }
+        }
+
+        return "Today"
+    }
+
+
+
+    // NSDate(dateString: "2014-06-06")
     convenience init(dateString: String, dateFormat: String = "yyyy-MM-dd") {
         let dateStringFormatter = NSDateFormatter()
         dateStringFormatter.dateFormat = dateFormat
@@ -68,6 +97,5 @@ extension NSDate {
         let date = dateStringFormatter.dateFromString(dateString)!
         self.init(timeInterval: 0, sinceDate: date)
     }
-    // NSDate(dateString: "2014-06-06")
-
+    
 }
