ff2782da012afa0dd08fb51cb9dd502d71b3cd99
jam891 <jam891@gmail.com>
2016-10-25 18:02:41 +0300

    fixed bug with forgot password on login screen

--- Pals/Classes/UI/Login/PLLoginViewController.swift
+++ Pals/Classes/UI/Login/PLLoginViewController.swift
@@ -7,87 +7,23 @@
 //
 
 class PLLoginViewController: PLViewController {
-	
-	@IBOutlet weak var mySpinner: UIActivityIndicatorView!
-	private var currentTextField: PLFormTextField!
 
-	@IBOutlet weak var scrollView: UIScrollView!
-	@IBOutlet weak var textFieldsView: UIView!
-	@IBOutlet weak var loginView: UIView!
-	@IBOutlet weak var logoImage: UIImageView!
+	@IBOutlet weak var logoImage:  PLCircularImageView!
 	@IBOutlet weak var loginTextField: PLFormTextField!
-	@IBOutlet weak var passTextField: PLFormTextField!
-	@IBOutlet var registerButton: UIButton!
+	@IBOutlet weak var passTextField:  PLFormTextField!
     
-	@IBOutlet var logoCenterXC: NSLayoutConstraint!
-    @IBOutlet var loginViewBotC: NSLayoutConstraint?
-	@IBOutlet var registerBotC: NSLayoutConstraint!
-	@IBOutlet var logoBottomC: NSLayoutConstraint!
-	
-    override func prefersStatusBarHidden() -> Bool {
-        return false
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        hideKeyboardWhenTapped = true
     }
     
-	@IBAction func loginButtonClicked(sender: AnyObject) {
-		loginToMainScreen()
-	}
-	
-	@IBAction func forgotButtonClicked(sender: AnyObject) {
+    
+    // MARK: - Private Methods
 		
-		let alert = UIAlertController(title: "We got your back!", message: "Enter below and we'll send your password!", preferredStyle: UIAlertControllerStyle.Alert)
-        
-		alert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil))
-        let forgotAction = UIAlertAction(title: "OK", style: .Default, handler: {[unowned self] (action) -> Void in
-            let textField = alert.textFields![0] as UITextField
-            NSNotificationCenter.defaultCenter().removeObserver(textField)
-            
-            if textField.text!.trim().isValidEmail {
-                self.mySpinner!.startAnimating()
-                PLFacade.sendPassword(textField.text!, completion: { (error) in
-                    if error == nil {
-                        PLShowAlert(title:"A new generated password has been sent to your email.")
-                    } else {
-                        PLShowErrorAlert(error: error!)
-                    }
-                    self.mySpinner?.stopAnimating()
-                })
-            } else {
-                PLShowAlert(title: "Please, enter a valid user email.")
-            }
-        })
-        forgotAction.enabled = false
-        
-		alert.addTextFieldWithConfigurationHandler({ (textField) in
-			textField.placeholder = "Email"
-            textField.keyboardType = .EmailAddress
-            NSNotificationCenter.defaultCenter().addObserverForName(UITextFieldTextDidChangeNotification, object: textField, queue: NSOperationQueue.mainQueue()) { (notification) in
-                forgotAction.enabled = textField.text != ""
-            }
-		})
-        
-		alert.addAction(forgotAction)
-		presentViewController(alert, animated: true, completion: nil)
-	}
-    
-    //FB
-    @IBAction func facebookLoginButtonPressed(sender: UIButton) {
-        startActivityIndicator(.WhiteLarge)
-        PLFacade.loginFB { [unowned self] (error) in
-            self.stopActivityIndicator()
-            if error == nil {
-                self.showMainScreen()
-            } else {
-                PLShowErrorAlert(error: error!)
-            }
-        }
-    }
-
-	// MARK: - Navigation
-    
-	@IBAction func unwindToLoginClicked(sender: UIStoryboardSegue) {
-	}
-	
-	func loginToMainScreen() {
+	private func loginToMainScreen() {
         var userName = ""
         var password = ""
         if loginTextField.text!.isEmpty && passTextField.text!.isEmpty {
@@ -100,116 +36,45 @@ class PLLoginViewController: PLViewController {
             password = passTextField.text!
         }
 
-		if userName.isEmpty {
-			PLShowAlert("Login error!", message: "Please enter your login.")
-		} else if password.isEmpty {
-			PLShowAlert("Login error!", message: "Please enter your password.")
-		} else {
-			self.view.userInteractionEnabled = false
-			mySpinner!.startAnimating()
-			PLFacade.login(userName, password: password, completion: { (error) in
-				if error != nil {
-					PLShowAlert("Login error!", message: (error?.localizedDescription)!)
-					self.view.userInteractionEnabled = true
-					self.mySpinner?.stopAnimating()
-				} else {
-					self.showMainScreen()
-					self.view.userInteractionEnabled = true
-					self.mySpinner?.stopAnimating()
-				}
+		if userName.isEmpty      { PLShowAlert("Login error!", message: "Please enter your login.") }
+        else if password.isEmpty { PLShowAlert("Login error!", message: "Please enter your password.") }
+        else {
+            startActivityIndicator(.WhiteLarge)
+			PLFacade.login(userName, password: password, completion: { error in
+                self.stopActivityIndicator()
+                
+                guard error == nil else { return PLShowAlert("Login error!", message: (error?.localizedDescription)!) }
+                self.showMainScreen()
 			})
 		}
 	}
 	
-	func showMainScreen() {
+	private func showMainScreen() {
 		let storyboard = UIStoryboard(name: "Main", bundle: nil)
 		let vc = storyboard.instantiateViewControllerWithIdentifier("TabBarController")
-		presentViewController(vc, animated: true, completion: nil)
+		present(vc, animated: true)
 	}
-	
-    override func viewDidLoad() {
-        super.viewDidLoad()
-		
-		loginTextField.delegate = self
-		passTextField.delegate = self
-		
-		animateSplashToLogin()
-        hideKeyboardWhenTapped = true
+    
+    private func sendPassword(email: String) {
+        self.startActivityIndicator(.WhiteLarge)
+        PLFacade.sendPassword(email, completion: { [unowned self] error in
+            self.stopActivityIndicator()
+            
+            guard error == nil else { return PLShowErrorAlert(error: error!) }
+            PLShowAlert(title:"A new generated password has been sent to your email.")
+        })
     }
-	
-	
-	func animateSplashToLogin() {
-		
-		logoCenterXC.active = true
-		logoBottomC.active = false
-		registerBotC.constant = -(registerButton.bounds.height * 2)
-		loginViewBotC!.constant = -loginView.bounds.height
-		view.layoutIfNeeded()
-			
-		UIView.animateWithDuration(1, delay: 2.2, options: .CurveEaseOut, animations: {
-				self.logoCenterXC.active = false
-				self.logoBottomC.active = true
-				self.loginViewBotC?.constant = 0
-				self.registerBotC.constant = 0
-					self.view.layoutIfNeeded()
-		}, completion: nil)
-	}
-	
-	override func viewWillAppear(animated: Bool) {
-		super.viewWillAppear(animated)
-		
-		registerKeyboardNotifications()
-	}
-	
-	override func viewDidDisappear(animated: Bool) {
-		super.viewWillDisappear(animated)
-		
-		NSNotificationCenter.defaultCenter().removeObserver(self)
-	}
 
-	// MARK: - Notifications
-	
-	private func registerKeyboardNotifications() {
-		NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
-		NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
-	}
-	
-	// MARK: - Keyboard
-	
-	func keyboardWillShow(notification: NSNotification) {
-		updateViewAnimated(notification)
-	}
-	
-	func keyboardWillHide(notification: NSNotification) {
-		updateViewAnimated(notification)
-	}
-	
-	private func updateViewAnimated(notification: NSNotification) {
-		let userInfo = notification.userInfo!
-		let keyboardSize = userInfo[UIKeyboardFrameEndUserInfoKey]!.CGRectValue.size
-		let duration = userInfo[UIKeyboardAnimationDurationUserInfoKey]?.doubleValue
-		let curve = userInfo[UIKeyboardAnimationCurveUserInfoKey]?.unsignedIntegerValue
-		let options = UIViewAnimationOptions(rawValue: curve!<<16)
-		let keyboardVisible = notification.name == UIKeyboardWillShowNotification
-		
-		UIView.animateWithDuration(duration!, delay: 0.0, options: options, animations: {
-			self.registerButton.enabled = keyboardVisible ? false : true
-			self.registerButton.alpha = keyboardVisible ? 0 : 1
-			self.logoImage.alpha = keyboardVisible ? 0 : 1
-			self.loginView.layoutIfNeeded()
-			}, completion: nil)
-		
-		var visibleRect = loginView.frame
-		visibleRect.size.height -= keyboardSize.height
-		
-		let scrollPoint = keyboardVisible ? CGPointMake(0, visibleRect.size.height / 2) : CGPointZero
-		scrollView.setContentOffset(scrollPoint, animated: true)
-		
-	}
-	
 }
 
+
+// MARK: - UITextFieldDelegate
+
 extension PLLoginViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        textField.keyboardDistanceFromTextField = 40.0
+    }
 	
 	func textFieldShouldReturn(textField: UITextField) -> Bool {
 		let nextTag = textField.tag + 1
@@ -222,12 +87,58 @@ extension PLLoginViewController: UITextFieldDelegate {
 		return false
 	}
 	
-	func textFieldDidEndEditing(textField: UITextField) {
-		currentTextField = nil
-	}
-	
-	func textFieldDidBeginEditing(textField: UITextField) {
-		currentTextField = textField as! PLFormTextField
-	}
-	
+}
+
+
+// MARK: - Actions
+
+extension PLLoginViewController {
+
+    @IBAction func loginButtonClicked(sender: AnyObject) {
+        loginToMainScreen()
+    }
+    
+    @IBAction func forgotButtonClicked(sender: AnyObject) {
+        dismissKeyboard(sender)
+        
+        let alert = UIAlertController(title: "We got your back!",
+                                      message: "Enter below and we'll send your password!",
+                                      preferredStyle: .Alert)
+        
+        let forgotAction = UIAlertAction(title: "OK", style: .Default, handler: { [unowned self] action in
+            if let emailTextField = alert.textFields!.first where emailTextField.text!.trim().isValidEmail {
+                self.sendPassword(emailTextField.text!)
+            } else {
+                PLShowAlert(title: "Please, enter a valid user email.")
+            }
+        })
+        
+        alert.addAction(forgotAction)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        alert.addTextFieldWithConfigurationHandler { textField in
+            textField.keyboardType = .EmailAddress
+            textField.placeholder  = "Email"
+        }
+        
+        present(alert, animated: true)
+    }
+    
+    //FB
+    @IBAction func facebookLoginButtonPressed(sender: UIButton) {
+        startActivityIndicator(.WhiteLarge)
+        PLFacade.loginFB { [unowned self] error in
+            self.stopActivityIndicator()
+            if error == nil {
+                self.showMainScreen()
+            } else {
+                PLShowErrorAlert(error: error!)
+            }
+        }
+    }
+    
+    // MARK: - Navigation
+    
+    @IBAction func unwindToLoginClicked(sender: UIStoryboardSegue) {
+    }
+
 }
