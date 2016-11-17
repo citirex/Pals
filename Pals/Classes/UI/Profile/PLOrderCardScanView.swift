9fb8dd63ba2e26e2191117c5808db110d3d54fd1
jam891 <jam891@gmail.com>
2016-11-17 11:54:32 +0200

    added scanner to orderScanCardView

--- Pals/Classes/UI/Profile/PLOrderCardScanView.swift
+++ Pals/Classes/UI/Profile/PLOrderCardScanView.swift
@@ -6,8 +6,30 @@
 //  Copyright © 2016 citirex. All rights reserved.
 //
 
-class PLOrderCardScanView: UIView, PLOrderContainable {
+import SwiftQRCode
 
+class PLOrderCardScanView: UIView, PLOrderContainable {
+    
+    private var previewView: UIView!
+    private var errorLabel: UILabel!
+    private var checkmark: UIImageView!
+    private var scanOverlay: UIImageView!
+    
+    private var scanner: QRCode!
+    
+    private var didSetupConstraints = false
+    
+    var order: PLOrder? {
+        didSet {
+            if let order = order {
+                PLLog("Did set order \(order.id)")
+                checking()
+            }
+        }
+    }
+
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         initialize()
@@ -24,16 +46,69 @@ class PLOrderCardScanView: UIView, PLOrderContainable {
     
     func initialize() {
         backgroundColor = .yellowColor()
+        
+        previewView = UIView.newAutoLayoutView()
+        scanOverlay = UIImageView.newAutoLayoutView()
+        checkmark   = UIImageView.newAutoLayoutView()
+        errorLabel  = UILabel.newAutoLayoutView()
+        
+        scanOverlay.image = UIImage(named: "scan_overlay")
+        scanOverlay.contentMode = .ScaleAspectFit
+        
+        checkmark.image = UIImage(named: "green_check")
+        checkmark.contentMode = .ScaleAspectFit
+        checkmark.hidden = true
+        
+        errorLabel.text = "Wrong Location"
+        errorLabel.textAlignment = .Center
+        errorLabel.textColor = .whiteColor()
+        errorLabel.font = .systemFontOfSize(22)
+        errorLabel.backgroundColor = .redColor()
+        errorLabel.hidden = true
+        
+        scanOverlay.addSubview(checkmark)
+        previewView.addSubview(scanOverlay)
+        previewView.addSubview(errorLabel)
+        
+        addSubview(previewView)
     }
     
-    var order: PLOrder? {
-        didSet {
-            if let o = order {
-                PLLog("Did set order \(o.id)")
-                // enable scanner, check qr code
+    private func checking() {
+        if bounds == CGRectZero { layoutIfNeeded() }
+        
+        checkmark.hidden = true
+        errorLabel.hidden = true
+        
+        scanner = QRCode(autoRemoveSubLayers: false, lineWidth: 0)
+        scanner.prepareScan(previewView) { [unowned self] stringValue in
+            if stringValue == self.order!.place.QRcode {
+                self.checkmark.hidden = false
             } else {
-                // disable scanner
+                self.errorLabel.hidden = false
             }
         }
+        scanner.startScan()
     }
+    
+    override func updateConstraints() {
+        if !didSetupConstraints {
+            
+            previewView.autoPinEdgesToSuperviewEdges()
+            
+            scanOverlay.autoSetDimensionsToSize(CGSizeMake(200, 200))
+            scanOverlay.autoCenterInSuperview()
+            
+            checkmark.autoSetDimensionsToSize(CGSizeMake(60, 60))
+            checkmark.autoCenterInSuperview()
+
+            errorLabel.autoSetDimension(.Height, toSize: 50)
+            errorLabel.autoPinEdgeToSuperviewEdge(.Leading)
+            errorLabel.autoPinEdgeToSuperviewEdge(.Trailing)
+            errorLabel.autoPinEdgeToSuperviewEdge(.Bottom)
+        
+            didSetupConstraints = true
+        }
+        super.updateConstraints()
+    }
+
 }
